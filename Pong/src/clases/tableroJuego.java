8ecd53d965113c17891d584ecdb058822c29ffbc
Gabriel <78141406+l3lackJack@users.noreply.github.com>
2021-07-15 20:30:44 +0200

    Hacemos una pelota

--- Pong/src/clases/tableroJuego.java
+++ Pong/src/clases/tableroJuego.java
@@ -6,14 +6,26 @@
 package clases;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
 
 /**
  *
  * @author usuario
  */
-public class tableroJuego extends JPanel{
-   public tableroJuego(){
-   setBackground(Color.BLACK);
-           }
+public class tableroJuego extends JPanel {
+
+    public tableroJuego() {
+        setBackground(Color.BLACK);
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+        g2.fill(new Rectangle2D.Double(0,0,20,20));
+        
+    }
 }
